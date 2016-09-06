4a203cfa4e594df71afa981cc75af2180377babd
yxiao <i@imyxiao.com>
2016-09-06 12:09:30 +0800

    /t转空格

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
-
 cd /opt/api-blueprint
+sed -i 's/\t/\ \ /g' *.apib
+chmod 755 *
+
 # build api document
-find . -name "*.apib" | sed 's/.apib//' | xargs -i -t aglio -i {}.apib `echo $aglio` -o {}.html
+find . -name "*.apib" | sed 's/.apib//'|xargs -i -t aglio -i {}.apib `echo $aglio` -o {}.html
 rm -rf /usr/share/nginx/html/*
 cp -R *.html /usr/share/nginx/html/
 
