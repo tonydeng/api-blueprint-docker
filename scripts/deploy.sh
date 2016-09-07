c0d79bcf455092e6025731c07c36e46af0c4090c
Tony Deng <wolf.deng@gmail.com>
2016-09-07 18:53:38 +0800

    feat(1.0): 解决有无repository不同处理方式

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,22 @@
-cd /opt/api-blueprint
+# update api blueprint documet
+if [ -d /opt/api-blueprint ]
+then
+    cd /opt/api-blueprint
+
+    if [[ -z $repository ]]; then
+        git checkout -f
+        git clean -f
+        git pull
+    fi
+
+else
+        git clone $repository /opt/api-blueprint
+        cd /opt/api-blueprint
+fi
+
+# convert tab and space
 sed -i 's/\t/\ \ /g' *.apib
-chmod 755 *
+chmod 644 *.apib
 
 # build api document
 find . -name "*.apib" | sed 's/.apib//'|xargs -i -t aglio -i {}.apib `echo $aglio` -o {}.html
